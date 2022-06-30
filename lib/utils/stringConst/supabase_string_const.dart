1bf01d4b240a9a6d6e4cf36e1e7a872f3c3fe7ef
Neel Vaghani <neel.vaghani@joflee.com>
2022-06-30 10:22:14 +0530

    working in deep link

---
+++ lib/utils/stringConst/supabase_string_const.dart
@@ -0,0 +1,5 @@
+class SupaseStringConst {
+  static String projectURL = 'https://xxbgnirphaebwikarswm.supabase.co';
+  static String anonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4YmduaXJwaGFlYndpa2Fyc3dtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTY0Nzk0NjIsImV4cCI6MTk3MjA1NTQ2Mn0.hvuJo7ygcF3ELL3vdWzoFtECf-aTjdQAmqwub5AwkOE';
+}
